29c043e47d5b48a3ae789afe1ff2c86b1e0c49c0
dsh91698 <mettus@mail.ru>
2023-06-05 22:47:37 +0400

    refactor: add current date to hello-page

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +15,21 @@ public class HelloController {
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
+
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
 		messages.add("I'm Java pre-project. Задача 2.3.1");
-		messages.add("Monday 05 Jun 2023");
+		messages.add(getToday());
+//		messages.add("Monday 05 Jun 2023");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	public String getToday() {// Get the current date
+		LocalDate currentDate = LocalDate.now();
+		System.out.println("currentDate -> " + currentDate);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMM yyyy");
+		return currentDate.format(formatter);
 	}
 	
 }
