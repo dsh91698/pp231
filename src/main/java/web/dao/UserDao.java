8e5f54cdffc306ea0ebd5cf80b26e43bad456a1c
dsh91698 <mettus@mail.ru>
2023-06-05 22:18:43 +0400

    feat: CRUD methods add into UserDao interface

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -1,4 +1,21 @@
 package web.dao;
 
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import java.util.Collection;
+import java.util.List;
+
 public interface UserDao {
+
+    //CRUD
+    void addUser(User user); //create
+
+    User getById(Long id); //read
+
+    List<User> selectAllUsersFromDatabase(); //read
+
+    void updateUser(User user); //update
+
+    void deleteById(Long id); //delete
 }
