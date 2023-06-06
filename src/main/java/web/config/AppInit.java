7c59ed5eb17d249919542a9514e3f918e33913e9
dsh91698 <mettus@mail.ru>
2023-06-07 01:37:35 +0400

    feat: filter for PATCH DELETE  add into configuration

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,6 +1,10 @@
 package web.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -18,6 +22,16 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         return new Class<?>[]{
                 WebConfig.class
         };
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 
     /* Данный метод указывает url, на котором будет базироваться приложение */
