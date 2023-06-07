03e3fc8e12f632fe0087766ae6c80f75ebe7190f
dsh91698 <mettus@mail.ru>
2023-06-08 00:22:19 +0400

    refactor: add methods for real database, mock methods remove

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -1,46 +1,52 @@
 package web.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.ArrayList;
 import java.util.List;
 
+//@Transactional
 @Repository
 public class UserDaoImp implements UserDao {
-    public static int COUNTER = 0;
-    public List<User> userList = new ArrayList<>();
+    @PersistenceContext
+    private final EntityManager entityManager;
 
-    public UserDaoImp() {
-        userList.add(new User((long) ++COUNTER, "Kevin", "active", 95));
-        userList.add(new User((long) ++COUNTER, "Noah", "banned", 85));
-        userList.add(new User((long) ++COUNTER, "Bobby", "read-only", 75));
-        userList.add(new User((long) ++COUNTER, "Caleb", "active", 65));
-        userList.add(new User((long) ++COUNTER, "John", "registered", 55));
+    @Autowired
+    public UserDaoImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
+    @Transactional
     @Override
     public void addUser(User user) {
-        user.setId((long) ++COUNTER);
-        userList.add(user);
-
-
+        System.out.println("adding user: " + user);
+        entityManager.persist(user);
     }
 
     @Override
     public User getById(Long id) {
-        return userList.stream()
-                .filter(user -> user.getId() == id)
-                .findFirst()
-                .orElse(null); // if User with the specified ID was not found
+        User user = entityManager.find(User.class, id);
+        return user;
+//        TypedQuery<User> query = entityManager.createQuery(
+//                "SELECT user FROM User user WHERE user.id = :id", User.class);
+//        query.setParameter("id", id);
+//        System.out.println("getById; " + query.getResultList().get(0));
+//        return query.getResultList().get(0);
     }
 
 
     @Override
     public List<User> selectAllUsersFromDatabase() {
-        return userList;
+        TypedQuery<User> query = entityManager.createQuery("FROM User", User.class);
+        return query.getResultList();
     }
-
+    @Transactional
     @Override
     public void updateUser(User userUpdated, Long id) {
         User user = getById(id);
@@ -48,9 +54,12 @@ public class UserDaoImp implements UserDao {
         user.setUserAge(userUpdated.getUserAge());
         user.setUserStatus(userUpdated.getUserStatus());
     }
-
+    @Transactional
     @Override
     public void deleteById(Long id) {
-        userList.removeIf(user -> user.getId() == id);
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
     }
 }
